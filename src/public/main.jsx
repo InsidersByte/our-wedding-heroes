ea0e109b3e9f40d4cc074e2267b631c69c877fa5
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-10-31 20:33:17 +0000

    added material ui

--- src/public/main.jsx
+++ src/public/main.jsx
@@ -1,9 +1,23 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 import React from 'react'; //eslint-disable-line
 import ReactDOM from 'react-dom';
+import RaisedButton from 'material-ui/lib/raised-button';
 
+// jscs:disable validateQuoteMarks
 ReactDOM.render(
-    <h1>
-        Hello World
-    </h1>,
+    <div>
+        <h1>
+            Hello World
+        </h1>
+
+        <RaisedButton label="Default" />
+    </div>,
     document.getElementById('content')
 );
