63065cd760f351c9ab5d7ad8ad393521c9ff9937
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-11-02 14:50:47 +0000

    add text fields and buttons to login page

--- src/public/components/Login.jsx
+++ src/public/components/Login.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Card, CardTitle, CardText, CardActions, TextField, RaisedButton } from 'material-ui';
 
 class App extends React.Component {
     render() {
+        const mainStyle = {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+        };
+
         return (
-            <div>
-                <h1>Coming Soon!</h1>
-            </div>
+            <main style={mainStyle}>
+                <Card>
+                    <CardTitle
+                        title="Login"
+                        subtitle="enter your details"/>
+                    <CardText>
+                        <TextField hintText="Username" /><br/>
+
+                        <TextField hintText="Password" type="password" />
+                    </CardText>
+                    <CardActions>
+                        <RaisedButton label="Login" primary />
+                    </CardActions>
+                </Card>
+            </main>
         );
     }
 }
