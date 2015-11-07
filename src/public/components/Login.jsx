f52602b65f44894a574d740922f8c706c9b9ec2b
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-11-07 19:37:02 +0000

    updated login page

--- src/public/components/Login.jsx
+++ src/public/components/Login.jsx
@@ -1,72 +1,65 @@
 import React from 'react';
-import { Panel, Input, Button, ButtonToolbar } from 'react-bootstrap';
+import { Input, Button, ButtonToolbar, Jumbotron } from 'react-bootstrap';
 import auth from '../utils/auth';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
             email: '',
             password: '',
         };
     }
 
-    handleEmailChange(that) {
+    handleEmailChange() {
         // This could also be done using ReactLink:
         // http://facebook.github.io/react/docs/two-way-binding-helpers.html
-        return function handleEmailChange() {
-            that.setState({
-                email: that.refs.email.getValue(),
-            });
-        };
+        this.setState({
+            email: this.refs.email.getValue(),
+        });
     }
 
-    handlePasswordChange(that) {
+    handlePasswordChange() {
         // This could also be done using ReactLink:
         // http://facebook.github.io/react/docs/two-way-binding-helpers.html
-        return function handleEmailChange() {
-            that.setState({
-                password: that.refs.password.getValue(),
-            });
-        };
+        this.setState({
+            password: this.refs.password.getValue(),
+        });
     }
 
-    handleSubmit(that) {
-        return function handleSubmit() {
-            event.preventDefault();
+    handleSubmit() {
+        event.preventDefault();
 
-            auth
-                .login(that.state.email, that.state.password)
-                .then(() => {
-                    alert('You have logged in successfully!');
-                })
-                .catch((error) => {
-                    alert(`error: ${error}`);
-                });
-        };
+        auth
+            .login(this.state.email, this.state.password)
+            .then(() => {
+            })
+            .catch(() => {
+            });
     }
 
     render() {
-        const panelHeader = (
-            <h3>
-                Login
-            </h3>
-        );
-
         return (
             <div className="col-md-6 col-md-offset-3">
-                <Panel header={panelHeader} bsStyle="primary">
-                    <form onSubmit={this.handleSubmit(this)}>
-                        <Input type="email" label="Email Address" placeholder="Enter email" ref="email" onChange={this.handleEmailChange(this)}/>
+                <Jumbotron>
+                    <h1>Login</h1>
 
-                        <Input type="password" label="Password" placeholder="Enter password" ref="password" onChange={this.handlePasswordChange(this)}/>
+                    <form onSubmit={this.handleSubmit}>
+                        <Input type="email" label="Email Address" placeholder="Enter email" ref="email"
+                               onChange={this.handleEmailChange}/>
+
+                        <Input type="password" label="Password" placeholder="Enter password" ref="password"
+                               onChange={this.handlePasswordChange}/>
 
                         <ButtonToolbar>
                             <Button type="submit" bsStyle="primary">Login</Button>
                             <Button type="reset">Reset</Button>
                         </ButtonToolbar>
                     </form>
-                </Panel>
+                </Jumbotron>
             </div>
         );
     }
