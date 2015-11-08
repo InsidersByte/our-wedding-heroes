18c38677006c6766bc2c7d9a1aed1fa585ee81b7
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-11-08 21:46:02 +0000

    add some placeholders

--- src/public/components/Landing.jsx
+++ src/public/components/Landing.jsx
@@ -10,6 +10,24 @@ class Landing extends React.Component {
                         <h1 className="landing__header__content__header">Our Wedding</h1>
                     </div>
                 </header>
+                <section className="landing__section">
+                    <h1 className="landing__section__heading">A little bit about us</h1>
+                </section>
+                <section className="landing__section landing__section--primary">
+                    <h1 className="landing__section__heading">About our day</h1>
+                </section>
+                <section className="landing__section">
+                    <h1 className="landing__section__heading">Where is it</h1>
+                </section>
+                <section className="landing__section landing__section--primary">
+                    <h1 className="landing__section__heading">Requests for the day</h1>
+                </section>
+                <section className="landing__section">
+                    <h1 className="landing__section__heading">About our honeymoon</h1>
+                </section>
+                <section className="landing__section landing__section--primary">
+                    <h1 className="landing__section__heading">Honeymoon Gift List</h1>
+                </section>
             </div>
         );
     }
