09b6995a0c0450388c8964cc1ff0981f816978df
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-01-14 13:33:04 +0000

    return the remaining count for a gift item

--- server/models/honeymoonGiftListItem.js
+++ server/models/honeymoonGiftListItem.js
@@ -1,24 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const HoneymoonGiftListItemSchema = new Schema(
-    {
-        imageUrl: { type: String, required: true },
-        name: { type: String, required: true },
-        description: { type: String, required: true },
-        requested: { type: Number, min: 1, required: true },
-        price: { type: Number, required: true, min: 0 },
-        gifts: [{ type: Schema.Types.ObjectId, ref: 'Gift' }],
-    },
-    {
-        toObject: { virtuals: true },
-        toJSON: { virtuals: true },
-    });
-
-HoneymoonGiftListItemSchema
-    .virtual('remaining')
-    .get(function getRemaining() {
-        return this.requested;
-    });
+const HoneymoonGiftListItemSchema = new Schema({
+    imageUrl: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    requested: { type: Number, min: 1, required: true },
+    price: { type: Number, required: true, min: 0 },
+    gifts: [{ type: Schema.Types.ObjectId, ref: 'Gift' }],
+});
 
 module.exports = mongoose.model('HoneymoonGiftListItem', HoneymoonGiftListItemSchema);
--- server/routes/weddingProfile.js
+++ server/routes/weddingProfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const WeddingProfile = require('../models/weddingProfile');
 const HoneymoonGiftListItem = require('../models/honeymoonGiftListItem');
 const co = require('co');
@@ -8,8 +10,28 @@ module.exports = (app, express) => {
     router.route('/')
         .get(co.wrap(function* getWeddingProfile(req, res, next) {
             try {
-                const weddingProfile = yield WeddingProfile.findOne({}).lean().exec();
-                const honeymoonGiftList = yield HoneymoonGiftListItem.find({});
+                const weddingProfile = yield WeddingProfile.findOne({}).lean();
+                const honeymoonGiftList = yield HoneymoonGiftListItem
+                    .find({})
+                    .populate('gifts', 'quantity')
+                    .lean()
+                    .exec();
+
+                for (const honeymoonGiftListItem of honeymoonGiftList) {
+                    honeymoonGiftListItem.remaining = honeymoonGiftListItem.requested;
+
+                    if (!honeymoonGiftListItem.gifts) {
+                        continue;
+                    }
+
+                    let bought = 0;
+
+                    for (const gift of honeymoonGiftListItem.gifts) {
+                        bought += gift.quantity;
+                    }
+
+                    honeymoonGiftListItem.remaining -= bought;
+                }
 
                 weddingProfile.honeymoonGiftListItems = honeymoonGiftList;
 
