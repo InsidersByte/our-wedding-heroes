7fbb786ea5459aff8ba9052e88b8f41b9356c5b3
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-01-28 14:03:31 +0000

    add update user functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -38,7 +38,7 @@ module.exports = (app, express) => {
                 if (error.code === 11000) {
                     return res
                         .status(400)
-                        .json({ success: false, message: 'A user with that username already exists. ' });
+                        .json({ success: false, message: 'A user with that username already exists.' });
                 }
 
                 return next(error);
@@ -47,6 +47,41 @@ module.exports = (app, express) => {
 
 
     router.route('/:userId')
+        .put((co.wrap(function* updateUser(req, res, next) {
+            try {
+                req.checkParams('id').equals(req.params.id);
+                req.checkBody('name').notEmpty();
+                req.checkBody('username').isEmail();
+                req.checkBody('password').notEmpty();
+
+                const errors = req.validationErrors();
+
+                if (errors) {
+                    return res
+                        .status(400)
+                        .send(errors);
+                }
+
+                const user = yield User.findById(req.body._id);
+
+                user.name = req.body.name;
+                user.username = req.body.username;
+                user.password = req.body.password;
+
+                yield user.save();
+
+                res.json({ message: 'User updated!' });
+            } catch (error) {
+                if (error.code === 11000) {
+                    return res
+                        .status(400)
+                        .json({ success: false, message: 'A user with that username already exists.' });
+                }
+
+                return next(error);
+            }
+        })))
+
         .delete(co.wrap(function* deleteUser(req, res) {
             yield User.remove({ _id: req.params.userId });
 
