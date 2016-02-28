4b165c0d1abe840f8b7d54dc5c5884ac25409cd6
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-02-28 15:08:19 +0000

    changed the no match styling to match the rest of the site

--- public/components/NoMatch.jsx
+++ public/components/NoMatch.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import './NoMatch.styl';
+
 class NoMatch extends React.Component {
     render() {
         return (
-            <div className="container-fluid">
+            <div className="no-match">
                 <h1>Page not found</h1>
 
                 <p>Woops! Sorry, there is nothing to see here.</p>
