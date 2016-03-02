939f23ce17b701d37511471b807670f0e316f314
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-03-02 15:19:53 +0000

    add config for mail settings

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,15 @@
 module.exports = {
     port: process.env.PORT || 8080,
     database: process.env.DATABASE_URL || 'mongodb://localhost:27017/honeymoon-gift-list',
-    secret: 'ilovetheideaofmyhoneymoon',
+    secret: process.env.SECRET || 'ilovetheideaofmyhoneymoon',
+    mail: {
+        transport: 'SMTP',
+        options: {
+            service: process.env.EMAIL_SERVICE, // https://www.npmjs.com/package/nodemailer#using-well-known-services
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        },
+    },
 };
