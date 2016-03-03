4b1d88733426f1a89625e2e78b62092f2ecf648b
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-03-03 18:45:05 +0000

    allow site title to be configured

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     port: process.env.PORT || 8080,
     database: process.env.DATABASE_URL || 'mongodb://localhost:27017/honeymoon-gift-list',
     secret: process.env.SECRET || 'ilovetheideaofmyhoneymoon',
-    siteTitle: 'Our Wedding',
+    siteTitle: process.env.SITE_TITLE || 'Our Wedding',
     mail: {
         from: process.env.EMAIL_FROM,
         options: {
