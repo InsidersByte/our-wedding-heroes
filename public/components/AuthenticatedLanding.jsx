d1fe5c4b48797be3e2b665f7a359799451d3ea70
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-03-06 09:04:49 +0000

    changed the wording slightly

--- public/components/AuthenticatedLanding.jsx
+++ public/components/AuthenticatedLanding.jsx
@@ -27,8 +27,8 @@ class AuthenticatedLanding extends React.Component {
         const giftSetCount = this.state.info.giftSetCount || 0;
 
         const message = giftSetCount <= 0 ?
-            'There have been no gift sets since you last logged in' :
-            `There has been ${giftSetCount} gift set${giftSetCount === 1 ? '' : 's'} since you last logged in!`;
+            'There have been no new gift sets since you last logged in' :
+            `There has been ${giftSetCount} new gift set${giftSetCount === 1 ? '' : 's'} since you last logged in!`;
 
         return (
             <div style={{ textAlign: 'center' }}>
