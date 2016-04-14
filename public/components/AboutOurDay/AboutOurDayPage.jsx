58ee8f25e453f28c8d9df1e14d31692625fb3aea
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-04-14 20:56:01 +0100

    split webpack config again, created new markdown editor page component

--- public/components/AboutOurDay/AboutOurDayPage.jsx
+++ public/components/AboutOurDay/AboutOurDayPage.jsx
@@ -1,61 +1,12 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
-import AboutOurDayForm from './AboutOurDayForm';
 import AboutOurDayStore from '../../stores/AboutOurDayStore';
 import AboutOurDayActions from '../../actions/AboutOurDayActions';
+import MarkdownEditorPage from '../common/MarkdownEditorPage';
 
-class AboutOurDayPage extends React.Component {
-    constructor() {
-        super();
-
-        this.state = AboutOurDayStore.getState();
-
-        this.setAboutOurDayState = this.setAboutOurDayState.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.submit = this.submit.bind(this);
-    }
-
-    componentDidMount() {
-        AboutOurDayStore.listen(this.onChange);
-        AboutOurDayActions.fetch();
-    }
-
-    componentWillUnmount() {
-        AboutOurDayStore.unlisten(this.onChange);
-    }
-
-    onChange(state) {
-        this.setState(state);
-    }
-
-    setAboutOurDayState(event) {
-        this.setState({ aboutOurDay: event.target.value });
-    }
-
-    submit(event) {
-        event.preventDefault();
-        AboutOurDayActions.update(this.state);
-    }
-
-    render() {
-        return (
-            <Jumbotron>
-                <h1>About Our Day</h1>
-
-                <AboutOurDayForm
-                    aboutOurDay={this.state.aboutOurDay}
-                    onChange={this.setAboutOurDayState}
-                    onSubmit={this.submit}
-                    saving={this.state.saving}
-                />
-            </Jumbotron>
-        );
-    }
+function AboutOurDayPage() {
+    return (
+        <MarkdownEditorPage propKey="aboutOurDay" title="About Our Day" store={AboutOurDayStore} actions={AboutOurDayActions} />
+    );
 }
-
-AboutOurDayPage.propTypes = {
-    toastSuccess: React.PropTypes.func,
-    toastError: React.PropTypes.func,
-};
 
 export default AboutOurDayPage;
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,33 +1,40 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StatsPlugin = require('stats-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const PATHS = {
     PUBLIC: path.join(__dirname, 'public'),
-    BUILD: path.join(__dirname, 'build'),
+    DIST: path.join(__dirname, 'dist'),
     NODE_MODULES: path.join(__dirname, 'node_modules'),
 };
 
+const extractCss = new ExtractTextPlugin('vendor-[hash].min.css');
+const extractStyl = new ExtractTextPlugin('app-[hash].min.css');
+
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        PATHS.PUBLIC,
-    ],
+    entry: {
+        app: PATHS.PUBLIC,
+        vendor: [
+            'react', 'moment', 'react-bootstrap', 'react-dom', 'flux', 'react-router', 'react-toastr', 'superagent',
+            'classnames', 'jwt-decode',
+        ],
+    },
     output: {
-        path: PATHS.BUILD,
-        filename: '[name].js',
+        path: PATHS.DIST,
+        filename: '[name]-[hash].min.js',
     },
     module: {
         loaders: [
             {
                 test: /\.css$/,
-                loaders: ['style', 'css'],
+                loader: extractCss.extract('style', ['css', 'postcss']),
             },
             {
                 test: /\.styl$/,
-                loaders: ['style', 'css', 'stylus'],
-                include: PATHS.PUBLIC,
+                loader: extractStyl.extract('style', ['css', 'postcss', 'stylus']),
             },
             {
                 test: /\.(js|jsx)$/,
@@ -59,9 +66,20 @@ module.exports = {
             filename: 'index.html',
         }),
         new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+                screw_ie8: true,
+            },
+        }),
+        new StatsPlugin('webpack.stats.json', {
+            source: false,
+            modules: false,
+        }),
+        extractCss,
+        extractStyl,
     ],
+    postcss: () => [autoprefixer],
     resolve: {
         extensions: ['', '.js', '.jsx'],
     },
